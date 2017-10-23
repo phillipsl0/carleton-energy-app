f9b23f3e5b2988e08dff4111020b53ab2b3ccc1f
ATW <andrewt.woosnam@gmail.com>
2017-10-23 11:32:30 -0500

    Added app icon and assigned map view to zoom to Carleton automatically

--- ios/energyapp/energyapp/HeatMap/HeatMapViewController.swift
+++ ios/energyapp/energyapp/HeatMap/HeatMapViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
 class HeatMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let regionRadius: CLLocationDistance = 1000
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let initialLocation = CLLocation(latitude: 44.461628, longitude: -93.153266)
+
+        centerMapOnLocation(location: initialLocation)
     }
 
     override func didReceiveMemoryWarning() {
