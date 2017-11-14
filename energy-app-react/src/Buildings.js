61779d3f0d2dd22836370d8965b1e2bc98c434e4
jboltron3000 <boltonj@carleton.edu>
2017-11-14 14:48:44 -0600

    Fake Data

--- energy-app-react/src/Buildings.js
+++ energy-app-react/src/Buildings.js
@@ -1,55 +1,144 @@
 const buildings = [
     {
         name: 'Burton',
-        avatar: 'https://i.pinimg.com/originals/5f/08/83/5f08832ee298016cb9baa79e2a44d0c0.jpg'
+        avatar: 'https://i.pinimg.com/originals/5f/08/83/5f08832ee298016cb9baa79e2a44d0c0.jpg',   
+        buildingID: 1,
+        floors: 4,
+        //API calls here
+        bfloor1: [12, 11, 100],
+        bfloor2: [30, 19, 22],
+        bfloor3: [144, 121, 100],
+        bfloor4: [111, 1101, 1010],
     },
     {
         name: 'Sayles',
-        avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/sized_images_local/set110/488110/a39b9465187c3a6c5255046bc6381f74.jpg'
+        avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/sized_images_local/set110/488110/a39b9465187c3a6c5255046bc6381f74.jpg',
+        buildingID: 2,
+        floors: 2,
+        //API calls here
+        sfloor1: [162, 151, 1400],
+        sfloor2: [302, 149, 252],
     },
     {
        name: 'Severance',
-       avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/sized_images_local/set138/496138/dda9ea601b8c4166b0ee1cd3d86e6fbb.jpg'
+       avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/sized_images_local/set138/496138/dda9ea601b8c4166b0ee1cd3d86e6fbb.jpg',
+       buildingID: 3,
+       floors: 3,  
+       //API calls here
+       sefloor1: [102, 171, 10],
+       sefloor2: [350, 19, 22],
+       sefloor3: [144, 121, 100],
     },
     {
        name: 'Davis',
-       avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/images/645842.jpg'
+       avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/images/645842.jpg',
+       buildingID: 4,
+       floors: 4,
+       //API calls here
+       dfloor1: [12, 11, 100],
+       dfloor2: [30, 19, 22],
+       dfloor3: [144, 121, 100],
+       dfloor4: [111, 1101, 1010],
     },
     {
        name: 'Musser',
-       avatar: 'http://www.peoplesco.com/assets/images/construction-specialties/inshied_carleton-musser.jpg'
+       avatar: 'http://www.peoplesco.com/assets/images/construction-specialties/inshied_carleton-musser.jpg',
+       buildingID: 5,
+       floors: 4,
+       //API calls here
+       mfloor1: [12, 11, 100],
+       mfloor2: [30, 19, 22],
+       mfloor3: [144, 121, 100],
+       mfloor4: [111, 1101, 1010],
     },
     {
        name: 'Myers',
-       avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/sized_images_local/set088/496088/a1e01d7aac13ef62e2d4c92b2e12ac41.jpg'
+       avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/sized_images_local/set088/496088/a1e01d7aac13ef62e2d4c92b2e12ac41.jpg',
+       buildingID: 6,
+       floors: 4,
+       //API calls here
+       myfloor1: [12, 11, 100],
+       myfloor2: [30, 19, 22],
+       myfloor3: [144, 121, 100],
+       myfloor4: [111, 1101, 1010],
     },
     {
        name: 'Cassat',
-       avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/sized_images_local/set344/586344/19248e063227e0265fb66cd1379a2352.jpg'
+       avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/sized_images_local/set344/586344/19248e063227e0265fb66cd1379a2352.jpg',
+       buildingID: 7,
+       floors: 4,
+       //API calls here
+       cafloor1: [12, 11, 100],
+       cafloor2: [30, 19, 22],
+       cafloor3: [144, 121, 100],
+       cafloor4: [111, 1101, 1010],
     },
     {
         name: 'Memo',
-        avatar: 'http://www.lhbcorp.com/wp-content/uploads/2013/08/CarletonCollege_CassatMemorial_070353_H11_G1.jpg'
+        avatar: 'http://www.lhbcorp.com/wp-content/uploads/2013/08/CarletonCollege_CassatMemorial_070353_H11_G1.jpg',
+        buildingID: 8,
+        floors: 4,
+        //API calls here
+        mefloor1: [12, 11, 100],
+        mefloor2: [30, 19, 22],
+        mefloor3: [144, 121, 100],
+        mefloor4: [111, 1101, 1010],
     },
     {
         name: 'Nourse',
-        avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/images_local/1057376.jpg'
+        avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/images_local/1057376.jpg',
+        buildingID: 9,
+        floors: 3,
+        //API calls here
+        nfloor1: [12, 11, 100],
+        nfloor2: [30, 19, 22],
+        nfloor3: [144, 121, 100],
     },
     {
         name: 'Evans',
-        avatar: 'http://finance-commerce.com/files/2013/07/EvansHall5x.jpg'
+        avatar: 'http://finance-commerce.com/files/2013/07/EvansHall5x.jpg',
+        buildingID: 10,
+        floors: 5,
+        //API calls here
+        efloor1: [12, 11, 100],
+        efloor2: [30, 19, 22],
+        efloor3: [144, 121, 100],
+        efloor4: [111, 1101, 1010],
+        efloor5: [144, 121, 100],
     },
     {
         name: 'Goodhue',
-        avatar: 'https://emeraldcitybookreview.com/wp-content/uploads/sites/163/2015/10/8-GoodhueExt.jpg'
+        avatar: 'https://emeraldcitybookreview.com/wp-content/uploads/sites/163/2015/10/8-GoodhueExt.jpg',
+        buildingID: 11,
+        floors: 4,
+        //API calls here
+        gfloor1: [12, 11, 100],
+        gfloor2: [30, 19, 22],
+        gfloor3: [144, 121, 100],
+        gfloor4: [111, 1101, 1010],
     },
     {
         name: 'Watson',
-        avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/sized_images_local/set183/490183/8bc51bab3276c56a4ecc7398aeee40de.jpg'
+        avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/sized_images_local/set183/490183/8bc51bab3276c56a4ecc7398aeee40de.jpg',
+        buildingID: 12,
+        floors: 7,
+        //API calls here
+        wfloor1: [12, 11, 100],
+        wfloor2: [30, 19, 22],
+        wfloor3: [144, 121, 100],
+        wfloor4: [111, 1101, 1010],
+        wfloor5: [30, 19, 22],
+        wfloor6: [144, 121, 100],
+        wfloor7: [111, 1101, 1010],
     },
     {
         name: 'Scoville',
-        avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/images_local/1579342.jpg'
+        avatar: 'https://apps.carleton.edu/reason_package/reason_4.0/www/images_local/1579342.jpg',
+        buildingID: 11,
+        floors: 2,
+        //API calls here
+        scfloor1: [12, 11, 100],
+        scfloor2: [30, 19, 22],
     },
 ]
 
