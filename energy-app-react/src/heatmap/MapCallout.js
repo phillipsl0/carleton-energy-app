f9bd81c25e72de53d619b78270ab02f21a48b88e
Veronica Child <vachild@me.com>
2018-01-31 15:07:19 -0600

    Add buggy loading screen

--- energy-app-react/src/heatmap/MapCallout.js
+++ energy-app-react/src/heatmap/MapCallout.js
@@ -6,24 +6,29 @@ import { Button } from 'react-native-elements';
 //Callout styling tutorial: https://rationalappdev.com/santas-map-app-with-react-native/
 // Open source icons: https://material.io/icons/#ic_local_florist
 
-/*
-            <Image
-              style={styles.infoIcon}
-              source={require('./../assets/infoCircleIcon.png')}
-            />
-*/
-
 export default class MapCallout extends Component {
   render() {
-    const { name, image, number } = this.props;
+    const { name, image, number, utility } = this.props;
+    
+    // Decide what units to render
+    var units = ""
+    if (utility == 'electricity') {
+      units = "kWh"
+    } else if (utility == 'water') {
+      units = "gal"
+    }
+
     return (
       <View style={styles.container}>
         <View style={styles.bubble}>
           <View>
             <Text style={styles.name}>{name}</Text>
+            <Text>
             <Text style={styles.number}>{number}</Text>
+            <Text style={styles.unit}> {units}</Text>
+            </Text>
           </View>
-          <View style={{ justifyContent: 'center', alignItems: 'center', padding: 10, }}>
+          <View style={{ justifyContent: 'center', alignItems: 'center', padding: 5, }}>
             <Text style={styles.icon}> > </Text>
           </View>
         </View>
@@ -47,8 +52,9 @@ const styles = StyleSheet.create({
     borderRadius: 6,
     borderColor: '#ccc',
     borderWidth: 0.5,
-    padding: 15,
-    width: 135,
+    paddingLeft: 15,
+    paddingBottom: 15,
+    paddingTop: 15,
   },
   // Arrow below the bubble
   arrow: {
@@ -74,12 +80,19 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   // Building number
+  //grey:     color: '#9E9E9E',
   number: {
     fontSize: 30,
+    color: '#0B5091',
   },
   // Wedge icon
   icon: {
     fontSize: 30,
+    color: '#0B5091',
+  },
+  // Unit next to number
+  unit: {
+    fontSize: 16,
     color: '#0B5091',
   }
 });
