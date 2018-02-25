7e24e556554039faadfa5d06996127a69fe13705
Martin Green <greenm2@carleton.edu>
2018-02-24 22:38:47 -0600

    Updated code style on comparison pages

--- energy-app-react/src/buildings/ComparisonPage.js
+++ energy-app-react/src/buildings/ComparisonPage.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import { FlatList, AppRegistry, SectionList, StyleSheet, View, Text, Image, WebView, ScrollView, Picker } from 'react-native'
-import { StackNavigator, SafeAreaView } from 'react-navigation';
-import { List, Card, ListItem, Button, Avatar } from 'react-native-elements';
-import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
-import buildings, {getAvatar} from './Buildings';
-import IndividualBuiding from './IndividualBuilding';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Card} from 'react-native-elements';
+import { getAvatar } from './Buildings';
 import { GetStyle } from './../styling/Themes';
 import CurrTheme from './../styling/CurrentTheme';
-import BuildingComparison from './BuildingComparison';
-import { getCurrentBuildingUtilityConsumption, getUtilitiesList, getCurrentBuildingUtilityConsumptionGraphFormat} from './../helpers/ApiWrappers.js';
-import Graph from './../visualizations/Graph';
-import { default as CustomThemes } from './../visualizations/GraphThemes';
+import { getCurrentBuildingUtilityConsumption } from './../helpers/ApiWrappers.js';
+import { moderateScale } from './../helpers/Scaling';
 
 function doMath(building1value, building2value) {
-    var utilityPercentA = (building1value/(building1value + building2value))*100 + '%';
-    var utilityPercentB = (building2value/(building1value + building2value))*100 + '%';
-    value = new Array(2);
+    let utilityPercentA = (building1value / (building1value + building2value)) * 100 + '%';
+    let utilityPercentB = (building2value / (building1value + building2value)) * 100 + '%';
+    let value = new Array(2);
     value[0] = utilityPercentA;
     value[1] = utilityPercentB;
     return value;
@@ -42,14 +37,14 @@ class ComparisonPage extends Component {
         const buildingAheat = Math.round(getCurrentBuildingUtilityConsumption(this.props.navigation.state.params.building1,'Heat'));
         const buildingBheat = Math.round(getCurrentBuildingUtilityConsumption(this.props.navigation.state.params.building2,'Heat'));
 
-        var electricGraphdata = doMath(buildingAelectric, buildingBelectric);
-        var waterGraphdata = doMath(buildingAwater, buildingBwater);
-        var heatGraphdata = doMath(buildingAheat, buildingBheat);
+        let electricGraphdata = doMath(buildingAelectric, buildingBelectric);
+        let waterGraphdata = doMath(buildingAwater, buildingBwater);
+        let heatGraphdata = doMath(buildingAheat, buildingBheat);
 
         return (
             <ScrollView style={{backgroundColor: '#fafafa'}}>
                 <View style={{paddingTop:10}} />
-                <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', paddingLeft: 7, paddingRight: 7}}>
+                <View style={[styles.flexRow, {justifyContent: 'space-around', paddingLeft: 7, paddingRight: 7}]}>
                     <View>
                         <Card containerStyle={[themeStyles.card, themeStyles.shadowed, styles.card]}>
                             <View>
@@ -71,13 +66,10 @@ class ComparisonPage extends Component {
                 </View>
 
                 <View>
-                    <Card 
-                        title="Electricity"
-                        titleStyle={styles.title}
-                        containerStyle={[themeStyles.card, themeStyles.shadowed, styles.card]}>
-                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center',}}>
+                    <Card title="Electricity" titleStyle={styles.title} containerStyle={[themeStyles.card, themeStyles.shadowed, styles.card]}>
+                        <View style={[styles.flexRow, {marginTop: -10}]}>
                             <View><Text style={styles.text}>{buildingAelectric} kWh</Text></View>
-                            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', height: 30,}}>
+                            <View style={styles.graph}>
                                 <View style={{width: electricGraphdata[0], backgroundColor: '#447BB0',}} />
                                 <View style={{width: electricGraphdata[1], backgroundColor: '#0B5091',}} />
                             </View>
@@ -86,9 +78,9 @@ class ComparisonPage extends Component {
                     </Card>
 
                     <Card title="Water" titleStyle={styles.title} containerStyle={[themeStyles.card, themeStyles.shadowed, styles.card]}>
-                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center',}}>
+                        <View style={[styles.flexRow, {marginTop: -10}]}>
                             <View><Text style={styles.text}>{buildingAwater} gal</Text></View>
-                            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', height: 30,}}>
+                            <View style={styles.graph}>
                                 <View style={{width: waterGraphdata[0], backgroundColor: '#447BB0',}} />
                                 <View style={{width: waterGraphdata[1], backgroundColor: '#0B5091',}} />
                             </View>
@@ -97,9 +89,9 @@ class ComparisonPage extends Component {
                     </Card>
 
                     <Card title="Heat" titleStyle={styles.title} containerStyle={[themeStyles.card, themeStyles.shadowed, styles.card]}>
-                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center',}}>
+                        <View style={[styles.flexRow, {marginTop: -10}]}>
                             <View><Text style={styles.text}>{buildingAheat} kBTUs</Text></View>
-                            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', height: 30,}}>
+                            <View style={styles.graph}>
                                 <View style={{width: heatGraphdata[0], backgroundColor: '#447BB0',}} />
                                 <View style={{width: heatGraphdata[1], backgroundColor: '#0B5091',}} />
                             </View>
@@ -114,13 +106,6 @@ class ComparisonPage extends Component {
     }
 }
 
-const navStyles = StyleSheet.create({
-    header: {
-        backgroundColor: '#0B5091',
-    },
-});
-
-
 const styles = StyleSheet.create({
     card: {
         paddingTop: 5,
@@ -128,25 +113,15 @@ const styles = StyleSheet.create({
         paddingLeft: 5,
         paddingRight: 5,
     },
-    button: {
-        paddingTop: 20,
-        paddingLeft: 30,
-        paddingRight: 30,
-        paddingBottom: 200,
-    },
     h1text: {
         alignSelf: 'center',
-        // marginLeft: 5,
-        fontSize: 24,
-        // paddingBottom: 10,
+        fontSize: moderateScale(24),
         fontWeight: 'bold',
         color: '#447BB0',
     },
     h2text: {
         alignSelf: 'center',
-        // marginLeft: 5,
-        fontSize: 24,
-        // paddingBottom: 10,
+        fontSize: moderateScale(24),
         fontWeight: 'bold',
         color: '#0B5091',
     },
@@ -154,23 +129,29 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         marginLeft: 5,
         marginRight: 8,
-        fontSize: 14,
+        fontSize: moderateScale(14),
         paddingTop: 5,
         paddingBottom: 10,
-        // fontWeight: 'bold',
     },
     title: {
         marginBottom: 2,
     },
     listImg: {
-        height: 147,
-        width: 149,
+        height: moderateScale(150),
+        width: moderateScale(150),
         alignSelf: 'center',
     },
-    img: {
-        alignSelf: 'center',
-        height: 30,
-        width: 30,
+    graph: {
+        flex: 1, 
+        flexDirection: 'row', 
+        justifyContent: 'center', 
+        height: moderateScale(25),
+    },
+    flexRow: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
 });
 
