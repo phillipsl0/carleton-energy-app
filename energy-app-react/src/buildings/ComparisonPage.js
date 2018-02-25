c9a8b421a82d7cd11e37105ce8230b99fbc76007
Martin Green <greenm2@carleton.edu>
2018-02-24 20:55:42 -0600

    Much cleaned up Electricity card

--- energy-app-react/src/buildings/ComparisonPage.js
+++ energy-app-react/src/buildings/ComparisonPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, AppRegistry, SectionList, StyleSheet, View, Text, Image, WebView, Picker } from 'react-native'
+import { FlatList, AppRegistry, SectionList, StyleSheet, View, Text, Image, WebView, ScrollView, Picker } from 'react-native'
 import { StackNavigator, SafeAreaView } from 'react-navigation';
 import { List, Card, ListItem, Button, Avatar } from 'react-native-elements';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
@@ -46,23 +46,89 @@ class ComparisonPage extends Component {
         var waterGraphdata = doMath(buildingAwater, buildingBwater);
         var heatGraphdata = doMath(buildingAheat, buildingBheat);
         return (
-            <View style={{flex: 1, flexDirection: 'column', paddingTop: 20,}}>
-                <View style={{flex: 1, flexDirection: 'row',}}>
+            <ScrollView style={{flex: 1, flexDirection: 'column', paddingTop: 20,}}>
+                <View style={{flex: 0, flexDirection: 'row',}}>
+                    <View>
                     <Card containerStyle={[styles.card, themeStyles.card, themeStyles.flex]}>
-                        <View style={{width: 120, height: 100, alignSelf: 'center'}}>
-                            <Image style={styles.listImg}
+                        <View //style={{width: 120, height: 100, alignSelf: 'center'}}
+                        >
+                            <Image style={styles.listImg} //resizeMode="cover"
                                    source={{ uri: getAvatar(this.props.navigation.state.params.building1)}} />
                         </View>
                     </Card>
+                        <Text style={styles.h1text}> {this.props.navigation.state.params.building1} </Text>
+                    </View>
+                    <View>
                     <Card containerStyle={[styles.card, themeStyles.card, themeStyles.flex]}>
-                        <View style={{width: 120, height: 100, alignSelf: 'center'}}>
-                            <Image style={styles.listImg}
+                        <View //style={{width: 120, height: 100, alignSelf: 'center'}}
+                        >
+                            <Image style={styles.listImg} //resizeMode="cover"
                                    source={{ uri: getAvatar(this.props.navigation.state.params.building2)}} />
                         </View>
                     </Card>
+                        <Text style={styles.h2text}> {this.props.navigation.state.params.building2} </Text>
+                    </View>
                 </View>
-                <View style={{flex: 2, flexDirection: 'row',}}>
-                    <View style={{width: 130, height: 225}}>
+                <View style={{flex: 2, flexDirection: 'row', justifyContent: 'space-around',}}
+                >
+                </View>
+
+
+
+
+                <View //style={{flex: 2, flexDirection: 'column',}}
+                >
+                    <Card title="Electricity">
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around',}}>
+                            <View><Text style={styles.text}>{buildingAelectric} kWh</Text></View>
+                            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', height: 20, width: 100,}}>
+                                <View style={{width:'40%', backgroundColor: '#447BB0',}} />
+                                <View style={{width:'60%', backgroundColor: '#0B5091',}} />
+                            </View>
+                            <View><Text style={styles.text}> {buildingBelectric} kWh</Text></View>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View>
+                            <Text style={styles.text}> Water: </Text>
+                            <Text style={styles.text}> {buildingAwater} gal</Text>
+                        </View>
+                        <View style={{flex: 1, flexDirection: 'row', height: 50, width: 200,}}>
+                            <View style={{height: 50, width:20, backgroundColor: 'blue'}} >
+                                <Text></Text>
+                            </View>
+                            <View style={{height: 50, width:50, backgroundColor: '#0B5091',}} >
+                                <Text></Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.text}> Water: </Text>
+                            <Text style={styles.text}> {buildingBwater} gal</Text>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View>
+                            <Text style={styles.text}> Gas: </Text>
+                            <Text style={styles.text}> {buildingAheat} kBTU</Text>
+                        </View>
+                        <View>
+                            <View style={{height: 50, width:20, backgroundColor: 'blue'}} >
+                                <Text> </Text>
+                            </View>
+                            <View style={{height: 50, width:50, backgroundColor: 'yellow'}} >
+                                <Text> </Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.text}> Gas: </Text>
+                            <Text style={styles.text}> {buildingBheat} kBTU</Text>
+                        </View>
+                    </Card>
+
+
+
+
+                    {/*<View style={{width: 130, height: 225}}>
                         <Text style={styles.h1text}> {this.props.navigation.state.params.building1} </Text>
                         <View style={{width: 110, height: 110, alignSelf: 'center', paddingBottom: 30, paddingTop: 10}}>
                             <Card containerStyle={[styles.cards, themeStyles.card, themeStyles.flex]}>
@@ -85,14 +151,12 @@ class ComparisonPage extends Component {
                     </View>
 
                     <View style={{flex: 1, flexDirection: 'row', height: 50, width: 200,}}>
-
-                        <View style={{height: 50, width:electricGraphdata[0], backgroundColor: 'blue'}} >
+                        <View style={{height: 50, width:20, backgroundColor: 'blue'}} >
                             <Text> </Text>
                         </View>
-                        <View style={{height: 50, width:electricGraphdata[0], backgroundColor: 'yellow'}} >
+                        <View style={{height: 50, width:50, backgroundColor: 'yellow'}} >
                             <Text> </Text>
                         </View>
-
                     </View>
 
                     <View style={{flex: 1, flexDirection: 'row',}}>
@@ -111,11 +175,11 @@ class ComparisonPage extends Component {
                         <View style={{height: 50, width:heatGraphdata[0], backgroundColor: 'yellow'}} >
                             <Text> </Text>
                         </View>
-                    </View>
+                    </View>*/}
                 </View>
 
 
-                <View style={{width: 130, height: 225,}}>
+                {/*<View style={{width: 130, height: 225,}}>
                     <Text style={styles.h2text}> {this.props.navigation.state.params.building2} </Text>
                     <View style={{width: 110, height: 110, alignSelf: 'center', paddingBottom: 30, paddingTop: 10}}>
                         <Card containerStyle={[styles.cards, themeStyles.card, themeStyles.flex]}>
@@ -135,8 +199,8 @@ class ComparisonPage extends Component {
                             <Text style={styles.text}> {buildingBheat} kBTU</Text>
                         </Card>
                     </View>
-                </View>
-            </View>
+                </View>*/}
+            </ScrollView>
         );
     }
 }
@@ -199,10 +263,11 @@ const styles = StyleSheet.create({
     text: {
         alignSelf: 'center',
         marginLeft: 5,
+        marginRight: 8,
         fontSize: 14,
         paddingTop: 5,
         paddingBottom: 10,
-        fontWeight: 'bold',
+        // fontWeight: 'bold',
     },
 
     listImg: {
@@ -230,7 +295,7 @@ const styles = StyleSheet.create({
 });
 
 // <View style={{
-//        flex: 1,
+//        flex: 0,
 //        flexDirection: 'column',
 //        paddingTop: 25,
 //      }}>
